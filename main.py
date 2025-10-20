4b80dfc31107a6f60ddf596026c240e8e252c8b6
Lucas Troteseil <troteseil.lucas@gmail.com>
2025-10-20 10:19:47 +0200

    Add main.py scraper script and update project files

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ from bs4 import BeautifulSoup
 
 def main():
     response = reuqest.get('./index.html')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    print(soup.prettify())
 
-url = ""
+
+if __name__ == "__main__":
+    main()
+
